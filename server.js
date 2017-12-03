932b01e488bc9a5db695a02b743dbad27347a058
Kevin Dyer <dyer@telmate.com>
2017-12-03 15:23:07 -0800

    linked app to build folder of shuffleboard-app repo.

--- server.js
+++ server.js
@@ -2,44 +2,66 @@ const express = require('express');
 const http = require('http');
 const url = require('url');
 var path = require('path');
-const WebSocket = require('ws');
+// const WebSocket = require('ws');
 
+var io = require('socket.io');
+var allowedOrigins = "http://localhost:* http://127.0.0.1:*";
 
 const app = express();
 const server = http.createServer(app);
-const wss = new WebSocket.Server({ server });
+// const wss = new WebSocket.Server({ server });
 
-
-app.get('/', (req, res) => {
-  res.sendFile(path.join(__dirname + '/index.html'));
+var sio_server = io(server, {
+    origins: allowedOrigins,
+    // path : '/',
+    transports: [
+      'websocket', 
+      'flashsocket', 
+      'htmlfile', 
+      'xhr-polling', 
+      'jsonp-polling', 
+      'polling'
+    ]
 });
 
-//Basic example
-// wss.on('connection', function connection(ws, req) {
-//   const location = url.parse(req.url, true);
-//   // You might use location.query.access_token to authenticate or share sessions
-//   // or req.headers.cookie (see http://stackoverflow.com/a/16395220/151312)
+app.use(express.static(path.join(__dirname + '/app/build/client')));
 
-//   ws.on('message', function incoming(message) {
-//     console.log('received: %s', message);
-//   });
+app.get('*', (req, res) => {
+  console.log("get index.html!")
+  res.sendFile(path.join(__dirname + '/app/build/client/index.html'));
+});
 
-//   ws.send('something');
-// });
+app.get("/test", (req, res) => {
+  res.send('OK brah')
+})
 
 
-//Broadcast
-wss.on('connection', function connection(ws) {
-  ws.on('message', function incoming(data) {
-    // Broadcast to everyone else.
-    wss.clients.forEach(function each(client) {
-      if (client !== ws && client.readyState === WebSocket.OPEN) {
-        client.send(data);
-      }
-    });
-  });
+//TODO: need to emit when user disconnects
+let userCount = 0
+sio_server.on('connection', function(socket){
+  socket.broadcast.emit('USER_JOINED', {userCount: ++userCount});
+  socket.emit('USER_JOINED', {userCount: userCount});
+  console.log('a user connected, userCount: ', userCount);
+
+  socket.on('BROADCAST_BOARD_CONFIG', function(msg) {
+
+    // console.log("broadcast board config msg: ", msg)
+    socket.broadcast.emit('BROADCAST_BOARD_CONFIG', msg);
+  })
+
+  socket.on('BROADCAST_PUCKS', function(msg) {
+    socket.broadcast.emit('BROADCAST_PUCKS', msg);
+  })
+
+  socket.on('disconnect', function (reason) {
+    // socket.broadcast.emit('USER_LEFT', {userCount: --userCount});
+    userCount = 0
+    socket.broadcast.emit('USER_LEFT', {userCount: userCount});
+    console.log('a user DISconnected, userCount: ', userCount);
+  })
 });
 
 server.listen(8080, function listening() {
   console.log('Listening on %d', server.address().port);
+  console.log('address: ', server.address());
 });
