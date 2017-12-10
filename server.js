5511405783deba746f9bf53ee8540d402c9d8d5d
Kevin Dyer <dyer@telmate.com>
2017-12-09 20:56:28 -0800

    update build.

--- server.js
+++ server.js
@@ -4,25 +4,25 @@ const url = require('url');
 var path = require('path');
 // const WebSocket = require('ws');
 
-var io = require('socket.io');
-var allowedOrigins = "http://localhost:* http://127.0.0.1:* http://0.0.0.0:* http://192.168.0.101:*";
+// var io = require('socket.io');
+// var allowedOrigins = "http://localhost:* http://127.0.0.1:* http://0.0.0.0:* http://192.168.0.101:*";
 
 const app = express();
 const server = http.createServer(app);
 // const wss = new WebSocket.Server({ server });
 
-var sio_server = io(server, {
-    origins: allowedOrigins,
-    // path : '/',
-    // transports: [
-    //   'websocket', 
-    //   'flashsocket', 
-    //   'htmlfile', 
-    //   'xhr-polling', 
-    //   'jsonp-polling', 
-    //   'polling'
-    // ]
-});
+// var sio_server = io(server, {
+//     origins: allowedOrigins,
+//     // path : '/',
+//     // transports: [
+//     //   'websocket', 
+//     //   'flashsocket', 
+//     //   'htmlfile', 
+//     //   'xhr-polling', 
+//     //   'jsonp-polling', 
+//     //   'polling'
+//     // ]
+// });
 
 app.use(express.static(path.join(__dirname + '/build/client')));
 
@@ -33,31 +33,31 @@ app.get('*', (req, res) => {
 
 
 //TODO: need to emit when user disconnects
-let userCount = 0
-sio_server.on('connection', function(socket){
-  socket.broadcast.emit('USER_JOINED', {userCount: ++userCount});
-  socket.emit('USER_JOINED', {userCount: userCount});
-  console.log('a user connected, userCount: ', userCount);
+// let userCount = 0
+// sio_server.on('connection', function(socket){
+//   socket.broadcast.emit('USER_JOINED', {userCount: ++userCount});
+//   socket.emit('USER_JOINED', {userCount: userCount});
+//   console.log('a user connected, userCount: ', userCount);
 
-  socket.on('BROADCAST_BOARD_CONFIG', function(msg) {
+//   socket.on('BROADCAST_BOARD_CONFIG', function(msg) {
 
-    // console.log("broadcast board config msg: ", msg)
-    socket.broadcast.emit('BROADCAST_BOARD_CONFIG', msg);
-  })
+//     // console.log("broadcast board config msg: ", msg)
+//     socket.broadcast.emit('BROADCAST_BOARD_CONFIG', msg);
+//   })
 
-  socket.on('BROADCAST_PUCKS', function(msg) {
-    socket.broadcast.emit('BROADCAST_PUCKS', msg);
-  })
+//   socket.on('BROADCAST_PUCKS', function(msg) {
+//     socket.broadcast.emit('BROADCAST_PUCKS', msg);
+//   })
 
-  socket.on('disconnect', function (reason) {
-    // socket.broadcast.emit('USER_LEFT', {userCount: --userCount});
-    userCount -= 1
-    socket.broadcast.emit('USER_LEFT', {userCount: userCount});
-    console.log('a user DISconnected, userCount: ', userCount);
-  })
-});
-
-server.listen(65080, function listening() {
+//   socket.on('disconnect', function (reason) {
+//     // socket.broadcast.emit('USER_LEFT', {userCount: --userCount});
+//     userCount -= 1
+//     socket.broadcast.emit('USER_LEFT', {userCount: userCount});
+//     console.log('a user DISconnected, userCount: ', userCount);
+//   })
+// });
+const PORT = process.env.PORT || 8080;
+server.listen(PORT, function listening() {
   console.log('Listening on %d', server.address().port);
   console.log('address: ', server.address());
 });
